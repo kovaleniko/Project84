92170f9e23c35cf0c98c7de9d6e9f0ad577a471c
kovaleniko <kovaleniko@yandex.ru>
2021-10-13 19:56:42 +0300

    add struct Point and function for its

--- Проект84/2021.10.13.cpp
+++ Проект84/2021.10.13.cpp
@@ -10,7 +10,18 @@ struct Q {
 	int x;
 	char y[50];
 };
+struct Point {
+	double x, y;
+};
+Point getPoint() {
+	Point tmp = { Rand() % 10,Rand() % 10 };
+	return tmp;
+}
+void display(Point tmp) {
+	cout << "(" << tmp.x << "," << tmp.y << ")\n";
+}
 //система контроля верcий
+
 //cvs svn git
 int main() {
 	Q q;
